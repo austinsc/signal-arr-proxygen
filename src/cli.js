deba429e928aa22c0db0e01a597174d9117f4abd
Stephen Austin <stephen.austin@rovisys.com>
2015-11-13 10:40:24 -0500

    initial commit

---
+++ src/cli.js
@@ -0,0 +1,12 @@
+import edge from 'edge';
+
+const helloWorld = edge.func(function() {/*
+  async (input) => {
+    return ".NET Welcomes " + input.ToString();
+  }
+*/});
+
+helloWorld('JavaScript', function(error, result) {
+  if(error) throw error;
+  console.log(result);
+});
