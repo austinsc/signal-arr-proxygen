96c42b419846ae6ce8bf61506d124a23f98bd6db
Stephen Austin <stephen.austin@rovisys.com>
2015-11-17 11:10:27 -0500

    Release 0.0.2

--- dist/cli.js
+++ dist/cli.js
@@ -6,10 +6,6 @@ var _path = require('path');
 
 var _path2 = _interopRequireDefault(_path);
 
-var _cfonts = require('cfonts');
-
-var _cfonts2 = _interopRequireDefault(_cfonts);
-
 var _prettyjson = require('prettyjson');
 
 var _prettyjson2 = _interopRequireDefault(_prettyjson);
@@ -18,9 +14,9 @@ var _yargs = require('yargs');
 
 var _yargs2 = _interopRequireDefault(_yargs);
 
-var _utilities = require('./utilities');
+var _processor = require('./processor');
 
-var _HubScanner = require('./HubScanner');
+var _processor2 = _interopRequireDefault(_processor);
 
 var argv = _yargs2['default'].usage('Usage: $0 <command> <assembly> [options]').command('scan', 'scans the specified .NET assembly and prints the results to the console').command('json', 'generate a JSON file that describes the specified .NET assembly').command('code', 'generate a javascript source code file that describes the specified .NET assembly', function (y) {
   return y.option('t', {
@@ -62,95 +58,8 @@ var argv = _yargs2['default'].usage('Usage: $0 <command> <assembly> [options]').
   group: 'Me Code Generation Options: '
 }).help('help').epilog('For more information, go to https://github.com/RoviSys/signal-arr').locale('pirate').wrap(_yargs2['default'].terminalWidth()).argv;
 
-//console.dir(argv);
+argv.command = argv._[0];
+argv.assembly = argv._[1];
 
-var command = argv._[0];
-var assembly = argv._[1];
-
-var promise = (0, _HubScanner.scan)(assembly);
-switch (command) {
-  case 'scan':
-    promise = promise.then(function (result) {
-      return argv.f ? _prettyjson2['default'].render(result) : result.map(function (x) {
-        return Object.assign(x, { r: _prettyjson2['default'].render(x) });
-      });
-    });
-    break;
-  case 'json':
-    promise = promise.then(function (result) {
-      return argv.f ? JSON.stringify(result, null, 2) : result.map(function (x) {
-        return Object.assign(x, { r: JSON.stringify(x, null, 2) });
-      });
-    });
-    break;
-  case 'code':
-    var options = {
-      clientVar: argv.c,
-      pathToClient: argv.p
-    };
-    switch (argv.t) {
-      case 'redux-classic':
-        var reduxClassic = require('./ReduxClassicTemplate');
-        promise = promise.then(function (result) {
-          var templatized = result.map(function (x) {
-            return Object.assign(x, { r: reduxClassic(x, options) });
-          });
-          return argv.f ? templatized.map(function (y) {
-            return y.r;
-          }).join('\r\n') : templatized;
-        });
-        break;
-      case 'redux':
-        var redux = require('./ReduxTemplate');
-        promise = promise.then(function (result) {
-          var templatized = result.map(function (x) {
-            return Object.assign(x, { r: redux(x, options) });
-          });
-          return argv.f ? templatized.map(function (y) {
-            return y.r;
-          }).join('\r\n') : templatized;
-        });
-        break;
-    }
-    break;
-}
-
-if (argv.f || argv.d) {
-  var fonts = new _cfonts2['default']({
-    'text': 'signal-arr', //text to be converted
-    'font': 'block', //define the font face
-    'colors': '', //define all colors
-    'background': 'Black', //define the background color
-    'letterSpacing': 1, //define letter spacing
-    'space': true, //define if the output text should have empty lines on top and on the bottom
-    'maxLength': '10' //define how many character can be on one line
-  });
-}
-if (argv.h) {}
-
-if (argv.f) {
-  promise = promise.then(function (result) {
-    return (0, _utilities.writeFile)(command, _path2['default'].normalize(argv.f), result);
-  });
-} else if (argv.d) {
-  (function () {
-    var ext = command === 'json' ? '.json' : '.js';
-    promise = promise.then(function (results) {
-      return Promise.all(results.map(function (x) {
-        return (0, _utilities.writeFile)(command, argv.d + x.Name + ext, x.r);
-      }));
-    });
-  })();
-} else {
-  promise = promise.then(function (results) {
-    results.forEach(function (x) {
-      return x.r.split('\r\n').forEach(function (y) {
-        return console.log(y);
-      });
-    });
-    return results;
-  });
-}
-
-promise['catch'](console.error);
+(0, _processor2['default'])(argv);
 //# sourceMappingURL=cli.js.map
